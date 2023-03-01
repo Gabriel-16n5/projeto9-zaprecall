d9254afe21d77b763a32a375a67734767797bd0c
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-03-01 16:58:58 -0300

    Configurações iniciais da base de arquivos do projeto(definido lógica inicial para estruturação do mesmo)

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+      <MainContainer >
+
+      </MainContainer>
+  );
+}
+
+export default App;
+
+const MainContainer = styled.div`
+  max-width: 375px;
+  height: 667px;
+  background-color: #FB6B6B;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  `
