3aff429c3c4fad7b58f0c63ad5c47b83a2c02577
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-03-02 02:57:38 -0300

    Cocluido a renderização do deck(objeto) por map, permitindo assim qualquer deck que vier do servidor ou ser criado

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 import {MainContainer, Header, Img, H1, H2, Footer, H3, IconPlay, Pergunta} from "./AppStyle";
 import logo from "../assets/logo.png";
 import iconPlay from "../assets/seta_play.png";
 import React from "react";
+import mock from "../mock";
+let contador = 0;
 
 function App() {
-  const [concluidos, setConcluidos] = React.useState("0/8 CONCLUÍDOS");
+  const [concluidos, setConcluidos] = React.useState(`${contador}/8 CONCLUÍDOS`);
   return (
       <MainContainer >
         <Header>
@@ -13,10 +15,11 @@ function App() {
           <H1>ZapRecall</H1>
         </Header>
           <>
+          {mock.map((deck, i) => 
           <Pergunta>
-            <H3>Pergunta 1</H3>
+            <H3>Pergunta {i+1}</H3>
             <IconPlay src={iconPlay} />
-          </Pergunta>
+          </Pergunta>)}
           </>
         <Footer>
           <H2>{concluidos}</H2>
@@ -26,4 +29,3 @@ function App() {
 }
 
 export default App;
-
