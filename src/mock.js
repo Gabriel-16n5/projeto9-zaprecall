529d8c63e815cdd0f297c3136a00e5f060aea9d3
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-03-05 21:03:39 -0300

    Testando avaliador

--- src/mock.js
+++ src/mock.js
@@ -1,7 +1,7 @@
 const mock = [
 	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
-	// { question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
-	// { question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+	{ question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+	{ question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
 	// { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
 	// { question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
 	// { question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
